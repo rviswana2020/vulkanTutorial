259b8c34ff6db481ce1043d3bdc893344b387dd3
Ramesh Viswanathan <rviswana2020@gmail.com>
2022-04-08 08:00:29 -0700

    Vulkan Window Surface: Initial check-in

---
+++ vulkanWindowSurface/vulkanWindowSurface.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN     // enable glfw to include vulkan headers
+#include <GLFW/glfw3.h>
+
+/*------------------------------------------------------------------*/
+// Hello Triangle Application Class
+/*------------------------------------------------------------------*/
+
+class HelloTriangleApplication {
+
+    public:
+        // top level run function that
+        //      -- initializes all glfw/vulkan objects
+        //      -- run rendering of images onto the screen
+        //      -- cleansup glfw/vulkan objects
+        void run();
+
+    private:
+        void initWindow();              // glfw window init code
+        void setupDebugMessenger();     // debug messenger setup code
+        void createVulkanInstance();    // vulkan instance creation code
+        void pickPhysicalDevice();      // vulkan physical device code
+        void createLogicalDevice();     // vulkan logical device code
+        void initVulkan();              // vulkan init code
+        void mainLoop();                // main rendering loop
+        void cleanup();                 // cleanup/release all glfw/vulkan objects
+
+    private:
+        GLFWwindow *window = nullptr;            // screen to render images
+        // debug callback handle
+        VkDebugUtilsMessengerEXT debugMessenger = VK_NULL_HANDLE;
+        VkInstance instance = VK_NULL_HANDLE;    // vulkan instance
+        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+        VkDevice device = VK_NULL_HANDLE;
+        VkQueue graphicsQueue = VK_NULL_HANDLE;  //opaque handle to queue object
+};
+
+/*------------------------------------------------------------------*/
