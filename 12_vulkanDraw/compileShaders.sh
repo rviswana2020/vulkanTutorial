32d98ffcaff7c6123a49ffd4ae1508fda42b6c88
Ramesh Viswanathan <rviswana2020@gmail.com>
2022-04-24 08:02:56 -0700

    adds script to compile shader and invokes it as part of make

---
+++ 12_vulkanDraw/compileShaders.sh
@@ -0,0 +1,4 @@
+/usr/local/bin/glslc shaders/shader.vert -o shaders/vert.spv
+/usr/local/bin/glslc shaders/shader.frag -o shaders/frag.spv
+/usr/local/bin/glslc shaders/shader_1.vert -o shaders/vert_01.spv
+/usr/local/bin/glslc shaders/shader_1.frag -o shaders/frag_01.spv
